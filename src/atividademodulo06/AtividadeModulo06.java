7b997a64f008ab79be28b644ec2088bb330ca390
Victor Wanderley <victor.wanderley1709@gmail.com>
2021-01-10 09:14:59 -0300

    Criando a representação dos alunos

--- src/atividademodulo06/AtividadeModulo06.java
+++ src/atividademodulo06/AtividadeModulo06.java
@@ -1,21 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package atividademodulo06;
+import java.util.List;
+import java.util.ArrayList;
 
 /**
- *
- * @author Victor-Vanessa
+ * Criando uma lista de chamada com a visualização do nome do aluno
+ * e de suas presenças.
+ * @author Victor
  */
 public class AtividadeModulo06 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        List<String> alunos = new ArrayList();
+        
+        alunos.add("Lucas");
+        alunos.add("Eslovênia");
+        alunos.add("Caio");
+        alunos.add("Joana");
+        alunos.add("Yasmim");
+        alunos.add("Roberto");
+        alunos.add("Nivaldo");
+        alunos.add("Bartira");
     }
     
 }
